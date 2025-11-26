0ec63d68e12990a02235c2432ba94538cd912ad2
AH Alif <ahasanhabibalif99@gmail.com>
2025-11-26 22:58:32 +0600

    sample

---
+++ DotNet/DotNet/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using DotNet.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNet.Controllers
+{
+    public class StudentController : Controller
+    {
+        DotNetEntities db = new DotNetEntities();
+        // GET: Student
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            var d = db.Departments.ToList();
+            return View(d);
+        }
+        [HttpPost]
+        public ActionResult Registration(Student s)
+        {
+            db.Students.Add(s);
+            db.SaveChanges();
+            TempData["msg"] = "registration Successfull";
+            return RedirectToAction("List");
+        }
+        public ActionResult List() { 
+        var d = db.Students.ToList();
+            return View(d);
+        }
+        public ActionResult Delete(Student s)
+        {
+            var st = db.Students.Find(s.Id);
+            db.Students.Remove(st);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = db.Students.Find(id);
+            ViewBag.Depts = db.Departments.ToList();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            var data = db.Students.Find(s.Id);
+            db.Entry(data).CurrentValues.SetValues(s);
+            db.SaveChanges();
+            TempData["msg"] = "data updated";
+            return RedirectToAction("List");
+        }
+
+
+    }
+}
