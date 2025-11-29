b636d8b7a6457fdf6020ecd838a80837e7388519
AH Alif <ahasanhabibalif99@gmail.com>
2025-11-30 00:20:07 +0600

    Zero Hunger(NGO practice) added

---
+++ NGO/NGO/NGO/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using NGO.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace NGO.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        NGOEntities db = new NGOEntities();
+
+        [HttpGet]
+        public ActionResult Registration() { return View(new Employee()); }
+        // GET: Employee
+        [HttpPost]
+        public ActionResult Registration(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            TempData["msg"] = "Employee Registration Successfully";
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List() 
+        {
+            var data = db.Employees.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult CollectRequestList()
+        {
+            var data = db.CollectRequests.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult EditCollectionRequest(int id)
+        {
+            var data = db.CollectRequests.Find(id);
+            ViewBag.emp = db.Employees.ToList();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult EditCollectionRequest(CollectRequest cr)
+        {
+            var data = db.CollectRequests.Find(cr.RequestId);
+            data.EmployeeId = cr.EmployeeId;
+            data.Status = cr.Status;
+            db.SaveChanges();
+            TempData["msg"] = "Status updated Successfully";
+            return RedirectToAction("CollectRequestList");
+        }
+
+
+    }
+}
---
+++ NGO/NGO/NGO/Controllers/RestaurantController.cs
@@ -0,0 +1,49 @@
+﻿using NGO.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NGO.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        NGOEntities db = new NGOEntities();
+        // GET: Restaurant
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Restaurant());
+        }
+        [HttpPost]
+        public ActionResult Create(Restaurant r)
+        {
+            db.Restaurants.Add(r);
+            db.SaveChanges();
+            TempData["msg"] = "Restaurant Registration Successfully";
+            return RedirectToAction("List");
+        }
+        public ActionResult List()
+        {
+            var data = db.Restaurants.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult CreateCollectRequest()
+        {
+            var data = db.Restaurants.ToList();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult CreateCollectRequest(CollectRequest cr)
+        {
+            db.CollectRequests.Add(cr);
+            db.SaveChanges();
+            TempData["msg"] = "Collect Request Added Successfully";
+            return RedirectToAction("List");
+        }
+    }
+}
